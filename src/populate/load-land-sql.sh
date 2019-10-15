9119b5f0e4702f089586bdeccae9b96e7d77b516
Ag Stephens <ag.stephens@stfc.ac.uk>
2019-10-15 10:41:04 +0100

    Updated table name to: "height_above_surface".

--- src/populate/load-land-sql.sh
+++ src/populate/load-land-sql.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-sqls=../sqls/land/*.sql
+REPORT_TYPE=$1
 
+if [ ! $REPORT_TYPE ] || [[ ! $REPORT_TYPE =~ ^[023]$ ]]; then
+    echo "[ERROR] Must provide report type of: 0, 2 or 3."
+    exit
+fi
 
-for sql in $sqls; do
+BASE_SQL_DIR=/gws/nopw/j04/c3s311a_lot2/data/cdmlite/land/sql
+sql_dir=$BASE_SQL_DIR/$REPORT_TYPE
+LOG_DIR=/gws/nopw/j04/c3s311a_lot2/data/cdmlite/land/populate
 
-    echo "[INFO] Loading: $sql"
+mkdir -p $LOG_DIR
+
+for sql in $(ls $sql_dir | sort -r); do
+
+    log=$LOG_DIR/${sql}.log
+    sql_file=$sql_dir/$sql
+
+    echo "[INFO] Loading data from: $sql_file"
+    echo "[INFO] Logging to: $log"
+
+    psql -U glamod_dbroot -h localhost cdmlite -f $sql_file > $log 2>&1 
 
 done
