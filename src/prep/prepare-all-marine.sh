a06b93c6138507cf5843d0a2ba8aa17c35128776
agstephens <ag.stephens@stfc.ac.uk>
2019-10-17 13:42:44 +0100

    Updated wallclock time to 6hrs

--- src/prep/prepare-all-marine.sh
+++ src/prep/prepare-all-marine.sh
@@ -22,7 +22,7 @@ for dr in $data_dirs; do
 
         sdir=$(basename $dr)
         logbase=${lotus_dir}/${sdir}
-        cmd="bsub -q short-serial -W 03:00 -o ${logbase}.out -e ${logbase}.err $cmd"
+        cmd="bsub -q short-serial -W 06:00 -o ${logbase}.out -e ${logbase}.err $cmd"
 
     fi
 
