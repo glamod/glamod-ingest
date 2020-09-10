cfe06364cd9a0cc1ce7af62fd9090e78b1f9df70
SteveDonegan <steve.donegan@stfc.ac.uk>
2020-09-10 16:45:33 +0100

    Interim commit to test picking up settings from glamod-config.py

--- src/populate/create-sqls-marine.sh
+++ src/populate/create-sqls-marine.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+script_dir=$(realpath $(dirname $0))
+BASEDIR=$(dirname $script_dir)
+config_script=${BASEDIR}/glamod-config.py
+
 REPORT_TYPE=$1
+
 
 if [ ! $REPORT_TYPE ] || [[ ! $REPORT_TYPE =~ ^[023]$ ]]; then
     echo "[ERROR] Must provide report type of: 0, 2 or 3."
@@ -8,9 +13,17 @@ if [ ! $REPORT_TYPE ] || [[ ! $REPORT_TYPE =~ ^[023]$ ]]; then
 fi
 
 #BASE_OUTPUT_DIR=/gws/nopw/j04/c3s311a_lot2/data/cdmlite/r201910/marine
-BASE_OUTPUT_DIR=/work/scratch-nompiio/astephen/glamod/r202001/cdmlite/marine
+#BASE_OUTPUT_DIR=/work/scratch-nompiio/astephen/glamod/r202001/cdmlite/marine
+BASE_OUTPUT_DIR=$($config_script ${release}:r2.0:lite:marine:outputs:workflow)
+
 #/${REPORT_TYPE}
-lotus_dir=/gws/smf/j04/c3s311a_lot2/ingest/log/populate/lotus-marine
+#lotus_dir=/gws/smf/j04/c3s311a_lot2/ingest/log/populate/lotus-marine
+lotus_dir=$($config_script ${release}:r2.0:lite:marine:outputs:lotus)
+
+echo $BASE_OUTPUT_DIR
+echo $lotus_dir
+
+exit #testing
 
 mkdir -p $lotus_dir
 
