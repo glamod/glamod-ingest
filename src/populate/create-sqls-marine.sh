ddb3b6bedd406193073cc21d635c4f6fd692b852
Stephen Donegan <sdonegan@sci2.jasmin.ac.uk>
2020-09-11 10:00:56 +0100

    Updated to use glamod-config.py

--- src/populate/create-sqls-marine.sh
+++ src/populate/create-sqls-marine.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-script_dir=$(realpath $(dirname $0))
+script_dir=$(realpath  $(pwd)/../../scripts/glamod-config.py)
 BASEDIR=$(dirname $script_dir)
 config_script=${BASEDIR}/glamod-config.py
 
 REPORT_TYPE=$1
+
+release=$2
+
+if [ $# != 2 ]
+then
+	echo "Usage: <report type> <release i.e. r2.0>"
+	exit
+fi
 
 
 if [ ! $REPORT_TYPE ] || [[ ! $REPORT_TYPE =~ ^[023]$ ]]; then
@@ -14,21 +22,16 @@ fi
 
 #BASE_OUTPUT_DIR=/gws/nopw/j04/c3s311a_lot2/data/cdmlite/r201910/marine
 #BASE_OUTPUT_DIR=/work/scratch-nompiio/astephen/glamod/r202001/cdmlite/marine
-BASE_OUTPUT_DIR=$($config_script ${release}:r2.0:lite:marine:outputs:workflow)
+BASE_OUTPUT_DIR=$($config_script ${release}:lite:marine:outputs:workflow)
 
 #/${REPORT_TYPE}
 #lotus_dir=/gws/smf/j04/c3s311a_lot2/ingest/log/populate/lotus-marine
-lotus_dir=$($config_script ${release}:r2.0:lite:marine:outputs:lotus)
-
-echo $BASE_OUTPUT_DIR
-echo $lotus_dir
-
-exit #testing
+lotus_dir=$($config_script ${release}:lite:marine:outputs:lotus)
 
 mkdir -p $lotus_dir
 
-mode=batch
-#mode=local
+#mode=batch
+mode=local
 
 for year in $(ls $BASE_OUTPUT_DIR | sort -r); do
 
@@ -42,6 +45,6 @@ for year in $(ls $BASE_OUTPUT_DIR | sort -r); do
     fi
 
     echo "[INFO] Running: $cmd"
-    $cmd
+    #$cmd
  
 done
